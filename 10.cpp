4815c9dece4a8750b75100c0e0afcaa4614db709
Thibaud <thibaud@debian>
2018-01-15 20:52:42 +0100

    Add problem #10

---
+++ 10.cpp
@@ -0,0 +1,34 @@
+/**
+ * @file 10.cpp
+ * This problem was asked by Apple.
+ * 
+ * Implement a job scheduler which takes in a 
+ * function f and an integer n, and calls f after n milliseconds.
+ */
+#include <thread>
+#include <chrono>
+
+
+using namespace std::chrono_literals;
+
+void delayedLaunch(auto&& f, unsigned int wait)
+{
+  std::chrono::milliseconds msecs(wait);
+  std::this_thread::sleep_for(msecs);
+  f();
+}
+
+int main(int argc, char** argv)
+{
+  try {
+    auto func = [](){
+      printf("Func has been called");
+    };
+    
+    delayedLaunch(func, atoi(argv[1]));
+  }catch(...) {
+    return 1;
+  }
+
+  return 0;
+};
